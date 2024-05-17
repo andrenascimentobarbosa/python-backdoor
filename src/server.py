3fd963a3c064cf0470128732cc8c463354032095
andrenascimentobarbosa <andrebarbosa2024@gmail.com>
2024-05-17 20:34:13 -0300

    working on it

--- src/server.py
+++ src/server.py
@@ -6,8 +6,7 @@ import socket
 import os
 from defs import *
 
-#host = '127.0.0.1'
-host = '172.22.254.158'
+host = '127.0.0.1'
 port = 8080
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
