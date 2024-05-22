4f9c4166a0197556ac92d657a8cd56f2f4e4c1bc
andrenascimentobarbosa <andrebarbosa2024@gmail.com>
2024-05-22 16:11:30 -0300

    simple

--- src/client.py
+++ src/client.py
@@ -2,52 +2,34 @@ import socket
 import os
 import subprocess
 
+
+def send_error(msg):
+    err_msg = f'[From client] {msg}'
+    client.send(err_msg.encode())
+
+
 host = '127.0.0.1'
 port = 8080
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((host, port))
 
-
-def send_error(msg):
-    error_msg = f'[From client] Error: {msg}'
-    client.send(error_msg.encode())
-
-
 while True:
-    try:
-        comm = client.recv(1024).decode()
-        if comm == 'close':
-            break
-        elif not comm:
-            break
-        elif comm[:3] == 'cd ':
-            try:
-                os.chdir(comm[3:])
-            except FileNotFoundError as e:
-                send_error(e)
-        elif comm[:5] == 'touch':
-            os.system(comm)
-        elif comm[:5] == 'mkdir':
-            os.system(comm)
-        elif comm[:5] == 'start':
-            os.system(comm)
-        elif comm[:4] == 'open':
-            os.system(comm) 
-        else:
-            try:
-                output = subprocess.run(comm, text=True, shell=True, capture_output=True)
-                if output.stdout:
-                    client.sendall(output.stdout.encode())
-                else:
-                    client.sendall('\nNO OUTPUT!'.encode())
-            except subprocess.CalledProcessError as e:
-                send_error(e)
-            except (ValueError, TypeError, AttributeError) as e:
-                send_error(e)
-    except socket.error as e:
-        send_error(e)
-        
+    comm = client.recv(1024).decode()
+    if comm == 'close':
+        break
+    elif comm[:3] == 'cd ':
+        try:
+            os.chdir(comm[3:])
+        except FileNotFoundError as e:
+            send_error(e)
+    else:
+        try:
+            output = subprocess.check_output(comm, text=True, shell=True)
+            if output:
+                client.send(output.encode())
+            else:
+                client.send('not output!'.encode())
+        except Exception as e:
+            send_error(e)
 client.close()
-
-
--- src/server.py
+++ src/server.py
@@ -1,7 +1,3 @@
-#!/usr/bin/python3
-
-# Server-side
-
 import socket
 import os
 
@@ -11,53 +7,25 @@ port = 8080
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen(1)
-print(f'\nlistening on \033[2m{port}...\033[m')
+print('listening on', port)
 
 client, addr = server.accept()
-print(f'\n\033[1;32m[+]\033[m connection establish: {addr[0]}:{addr[1]}')
+print('connected to', addr)
 
+while True:
+    comm = input(f'{addr}% ')
+    if comm == 'close':
+        client.send(comm.encode())
+        break
+    elif comm[:3] == 'cd ':
+        client.send(comm.encode())
+    elif comm == 'clean':
+        os.system(comm)
+    elif comm.strip() == '':
+        pass
+    else:
+        client.send(comm.encode())
+        output = client.recv(1024).decode()
+        print(output)
 
-def print_error(msg):
-    error_msg = f'Error: {msg}'
-    print(error_msg)
-
-
-def shell():
-    while True:
-        try:
-            comm = str(input(f'\033[1;35mshell\033[m~{addr[0]}: '))
-            if comm == '':
-                pass
-            elif comm == 'close':
-                client.send(comm.encode())
-                break
-            elif comm == 'clear':
-                os.system(comm)
-            elif comm[:3] == 'cd ':
-                client.send(comm.encode())
-            elif comm[:5] == 'mkdir':
-                client.send(comm.encode())
-            elif comm[:5] == 'touch':
-                client.send(comm.encode())
-            elif comm[:5] == 'start':
-                client.send(comm.encode())
-            elif comm[:4] == 'open':
-                client.send(comm.encode())
-            else:
-                try:
-                    client.send(comm.encode())
-                    output = client.recv(1024).decode()
-                    print(output)
-                except socket.error as e:
-                    print(f'Error: {e}')
-        except (ValueError, TypeError, IndexError) as e:
-            print(f'\nError: {e}')
-
-    server.close()
-    print('connection closed.')
-
-
-shell()
-
-
-
+server.close()
